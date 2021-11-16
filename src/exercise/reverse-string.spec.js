09c292052519a442e95e86b657eb3a76ed66abb8
TJ Maynes <tj@tjmaynes.com>
2021-11-16 09:19:44 -0500

    Create exercise directory

--- src/exercise/reverse-string.spec.js
+++ src/exercise/reverse-string.spec.js
@@ -1,8 +1,7 @@
 const reverseString = (input) => {
     let result = "";
-    for(let i = input.length - 1; i >= 0; i--) {
+    for (let i = input.length - 1; i >= 0; i--)
         result += input[i];
-    }
 
     return result;
 }
